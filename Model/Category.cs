3b39a8dc7c0b5a0a985b74581e8bfde43d04a442
Farhad <farhadalam75@gmail.com>
2021-10-07 01:44:27 +0600

    remove unnecessary usings

--- Model/Category.cs
+++ Model/Category.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Microservice.NET.Core.Model
+﻿namespace Microservice.NET.Core.Model
 {
     public class Category
     {
