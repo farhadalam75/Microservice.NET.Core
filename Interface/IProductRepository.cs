caa65fefdaec9080e363fd6f54cd6fa3e2e9fa0e
Farhad <farhadalam75@gmail.com>
2021-10-07 01:40:27 +0600

    interface added

---
+++ Interface/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using Microservice.NET.Core.Model;
+using System.Collections.Generic;
+namespace Microservice.NET.Core.Repository
+{
+    internal interface IProductRepository
+    {
+        void DeleteProduct(int productId);
+        Product GetProductByID(int productId);
+        IEnumerable<Product> GetProducts();
+        void InsertProduct(Product product);
+        void Save();
+        void UpdateProduct(Product product);
+    }
+}
