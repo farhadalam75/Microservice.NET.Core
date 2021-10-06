e0d6f6b2f000c629206c86dc1584a39178e72e90
Farhad <farhadalam75@gmail.com>
2021-10-07 01:26:23 +0600

    sql server db provider for ef core added

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,8 @@
+using Microservice.NET.Core.DBContext;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +27,7 @@ namespace Microservice.NET.Core
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<ProductContext>(o => o.UseSqlServer(Configuration.GetConnectionString("ProductDB")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
